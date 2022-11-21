1f81e25a70ad990b3af248b244cecdd4e5af5633
Rislan <his.lan1998@gmail.com>
2022-11-21 20:01:44 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/questao.Dart.dart';
+//import './questao.dart';
+main () => runApp (PerguntaApp());
+  class _PerguntaAppState extends State <PerguntaApp>{
+    var _perguntaSelecionada = 0;
+    void _responder (){
+      setState(() {
+      _perguntaSelecionada++;
+      });
+      print (_perguntaSelecionada);
+    }
+    @override
+  Widget build(BuildContext context ) {
+      final List<String> perguntas = [
+        'Qual é sua cor favorita?',
+        'Qual é o seum animal favorit?',
+      ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+           centerTitle: true,
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children:<Widget>[
+            Questao(perguntas[_perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta1'),
+              onPressed: _responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta2'),
+              onPressed: _responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta3'),
+              onPressed: _responder,
+            ),
+          ]
+        )
+      )
+    );
+  }
+}
+
+  class PerguntaApp extends StatefulWidget {
+    @override
+    _PerguntaAppState createState() {
+      return _PerguntaAppState();
+    }
+
+  }
+
+
